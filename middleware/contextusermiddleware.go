88b1fd95f6cc4a2fb3e392be6cdbe2e315c1410e
xiangxu <xiangxu@innotechx.com>
2020-01-17 15:58:27 +0800

    add two

---
+++ middleware/contextusermiddleware.go
@@ -0,0 +1,29 @@
+package http
+
+import (
+	"encoding/json"
+
+	"github.com/echoturing/log"
+	"github.com/labstack/echo"
+)
+
+const HeaderUserID = "X-User-Id"
+
+func WrapContextWithUser() func(echo.HandlerFunc) echo.HandlerFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			resp := c.Response()
+			userID := req.Header.Get(HeaderUserID)
+			requestID := resp.Header().Get(echo.HeaderXRequestID)
+			ctx := c.Request().Context()
+			requestIDUser := log.RequestIDWithUser{
+				UserID:    json.Number(userID),
+				RequestID: requestID,
+			}
+			ctx = log.NewContext(ctx, requestIDUser)
+			c.SetRequest(req.WithContext(ctx))
+			return next(c)
+		}
+	}
+}
---
+++ middleware/handlewrapper.go
@@ -0,0 +1,47 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/echoturing/log"
+	"github.com/labstack/echo"
+)
+
+type APICode int
+
+const (
+	APICodeOK  APICode = 0
+	APICodeErr APICode = 500
+)
+
+type HandlerFunc func(c echo.Context) (data interface{}, err error)
+
+func HandlerFuncWrapper(fn HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		type response struct {
+			Code      int         `json:"code"`
+			Message   string      `json:"msg"`
+			RequestID string      `json:"request_id"`
+			Data      interface{} `json:"data"`
+		}
+		resp := response{
+			Code:    int(APICodeOK),
+			Message: "",
+		}
+		ctx := c.Request().Context()
+		requestIDUserID := log.FromContext(ctx)
+		data, err := fn(c)
+		responseContentType := c.Response().Header().Get(echo.HeaderContentType)
+		if responseContentType == echo.MIMEOctetStream {
+			// 如果是stream。那就直接返回nil了。。因为data肯定是nil
+			return nil
+		}
+		if err != nil {
+			resp.Code = int(APICodeErr)
+			resp.Message = err.Error()
+		}
+		resp.Data = data
+		resp.RequestID = requestIDUserID.RequestID
+		return c.JSON(http.StatusOK, &resp)
+	}
+}
